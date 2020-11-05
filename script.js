b6f9c8feee1e046ae751b7716cf069b0ff04ff86
lyssacavalcanti <lyssa.cavalcanti@gmail.com>
2020-11-05 10:29:53 -0300

    Worked on CSS button elements and started JS file.

---
+++ script.js
@@ -0,0 +1,9 @@
+const pw = document.getElementById('pw');
+const copy = document.getElementById('copy');
+const len = document.getElementById('len');
+const upper = document.getElementById('upper');
+const lower = document.getElementById('lower');
+const number = document.getElementById('number');
+const symbol = document.getElementById('symbol');
+const generate = document.getElementById('generate');
+
