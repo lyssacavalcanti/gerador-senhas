ab47f2fe0c9ab4d70d3644bc3a8b9e248ebc3d07
lyssacavalcanti <lyssa.cavalcanti@gmail.com>
2020-11-05 22:33:19 -0300

    Inserted functions and conditional to generate passwords.

--- script.js
+++ script.js
@@ -1,9 +1,66 @@
-const pw = document.getElementById('pw');
-const copy = document.getElementById('copy');
-const len = document.getElementById('len');
-const upper = document.getElementById('upper');
-const lower = document.getElementById('lower');
-const number = document.getElementById('number');
-const symbol = document.getElementById('symbol');
-const generate = document.getElementById('generate');
+const pwEl = document.getElementById("pw");
+const copyEl = document.getElementById("copy");
+const lenEl = document.getElementById("len");
+const upperEl = document.getElementById("upper");
+const lowerEl = document.getElementById("lower");
+const numberEl = document.getElementById("number");
+const symbolEl = document.getElementById("symbol");
+const generateEl = document.getElementById("generate");
 
+const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerLetters = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "123456789";
+const symbols = "!@#$%ˆ&()_+";
+
+function getUppercase() {
+    return upperLetters[Math.floor(Math.random() * upperLetters.length)];
+}
+
+function getLowercase() {
+    return lowerLetters[Math.floor(Math.random() * lowerLetters.length)];
+}
+
+function getNumber() {
+    return numbers[Math.floor(Math.random() * numbers.length)];
+}
+
+function getSymbol() {
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+ function generatePassword() {
+     const len = lenEl.value;
+
+     let password = "";
+
+     for(let i=0; i<len; i++) {
+        const x = generateX(); 
+        password += x;
+     }
+
+     pwEl.innerText = password;
+ }
+
+function generateX() {
+    
+    const xs = [];
+    if (upperEl.checked) {
+        xs.push(getUppercase());
+    }
+
+    if (lowerEl.checked) {
+        xs.push(getLowercase());
+    }
+
+    if (numberEl.checked) {
+        xs.push(getNumber());
+    }
+
+    if (symbolEl.checked) {
+        xs.push(getSymbol());
+    }
+
+    return xs[Math.floor(Math.random() * xs.length)];
+}
+
+ generateEl.addEventListener("click", generatePassword);
