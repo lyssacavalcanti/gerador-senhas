5414e6dab6fff93932ac35cb04a2a167f182885e
lyssacavalcanti <lyssa.cavalcanti@gmail.com>
2020-11-05 23:32:33 -0300

    Coding updated in JS file.

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const numberEl = document.getElementById("number");
 const symbolEl = document.getElementById("symbol");
 const generateEl = document.getElementById("generate");
 
+//Opções de caracteres disponíveis para a senha
 const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerLetters = "abcdefghijklmnopqrstuvwxyz";
 const numbers = "123456789";
@@ -28,6 +29,7 @@ function getSymbol() {
     return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
+// Gera senha de acordo com a quantidade e tipo de caracteres
  function generatePassword() {
      const len = lenEl.value;
 
